9c357cfcc921759c4af589db2fb2f25ff30d5cf7
dianasaur323 <dianahsieh323@gmail.com>
2018-08-03 12:40:39 -0400

    adding request status checking

--- hn_rank/core.py
+++ hn_rank/core.py
@@ -4,15 +4,15 @@ from datetime import datetime
 
 def rank_urls(urls, year=None, filename=None):
 	"""
-	Takes a list of URLs and searches for them in 
+	Takes a list of URLs and searches for them in
 	Hacker News submissions. Prints or saves each
-	URL and its total points to a given filename 
-	in descending order of points. Searches for 
+	URL and its total points to a given filename
+	in descending order of points. Searches for
 	submissions from all years, unless year is given.
 	"""
 	now = datetime.now()
 	if year:
-		if year > now.year: 
+		if year > now.year:
 			print("Please enter a valid year parameter (example: " + str(now.year) + ") or leave out for all time.")
 			return None
 		else:
@@ -24,7 +24,8 @@ def rank_urls(urls, year=None, filename=None):
 	for url in urls:
 		query = 'http://hn.algolia.com/api/v1/search?query=' + url + '&restrictSearchableAttributes=url'
 		r = requests.get(query)
-		data = json.loads(r.text)
+		if r:
+			data = json.loads(r.text)
 		total_score = 0
 
 		for item in data['hits']:
@@ -52,7 +53,7 @@ def rank_urls(urls, year=None, filename=None):
 		for key, value in sorted_leaderboard:
 			f.write(str(value) + "," + key + '\n')
 		f.close()
-		print('Results saved to ' + filename)	
+		print('Results saved to ' + filename)
 	else:
 		for key, value in sorted_leaderboard:
 			print(str(value) + "," + key)
@@ -60,20 +61,20 @@ def rank_urls(urls, year=None, filename=None):
 
 def rank_articles(urls, year=None, filename=None):
 	"""
-	Takes a list of URLs and searches for them in 
-	Hacker News submissions. Prints or saves each article 
-	URL and its points to a given filename in descending 
-	order of points. Searches for submissions from all years, 
+	Takes a list of URLs and searches for them in
+	Hacker News submissions. Prints or saves each article
+	URL and its points to a given filename in descending
+	order of points. Searches for submissions from all years,
 	unless specific year is given.
 	"""
 	now = datetime.now()
 	if year:
-		if year > now.year: 
+		if year > now.year:
 			print("Please enter a valid year parameter (example: " + str(now.year) + ") or leave out for all time.")
 			return None
 		else:
 			pass
-		
+
 	articles = {}
 	count = 0
 
@@ -108,4 +109,3 @@ def rank_articles(urls, year=None, filename=None):
 	else:
 		for key, value in sorted_articles:
 			print(str(value) + "," + key)
-
